39006e603c4fa4ad06da3046c09591b62b53eb86
mericanAncer <vicenteputheany701@gmail.com>
2025-02-21 21:44:20 +0100

    removed all controllers

--- server.js
+++ server.js
@@ -2,11 +2,6 @@
 
 const express = require("express");
 
-const userRoutes = require("./routes/users");
-const groupRoutes = require("./routes/groups");
-const saleRoutes = require("./routes/sales");
-const userGroupRoutes = require("./routes/userGroups");
-
 // Constants
 const PORT = 5000;
 const HOST = "0.0.0.0";
@@ -33,11 +28,6 @@ async function start() {
   // Write your endpoints here
 
   // Register routes
-  app.use("/api/users", userRoutes); // User routes
-  app.use("/api/groups", groupRoutes); // Group routes
-  app.use("/api/sales", saleRoutes); // Sales routes
-  app.use("/api/userGroups", userGroupRoutes); // User-Group routes
-
   app.listen(PORT, HOST);
   console.log(`Server is running on http://${HOST}:${PORT}`);
 }
